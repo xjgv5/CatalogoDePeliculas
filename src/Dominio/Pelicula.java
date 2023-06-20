d0662f0bf14a540bcc620179ad286f072a3c64e3
Javier Gomez <javier.gov@outlook.es>
2023-06-19 22:56:17 -0600

    [UPDATE] creacion de la clase y paquete pelicula

---
+++ src/Dominio/Pelicula.java
@@ -0,0 +1,48 @@
+package Dominio;
+
+import java.util.Objects;
+
+public class Pelicula {
+    private String nombre;
+
+    public Pelicula(){}
+
+    public Pelicula(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pelicula pelicula = (Pelicula) o;
+
+        return Objects.equals(nombre, pelicula.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return nombre != null ? nombre.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Pelicula [ " + "Nombre : " + nombre + " ]";
+    }
+
+    public static void main(String[] args) {
+        Pelicula pelicula = new Pelicula("Batman");
+        Pelicula pelicula1 = new Pelicula("Spiderman");
+
+        System.out.println(pelicula1 + "\n" + pelicula);
+    }
+}
